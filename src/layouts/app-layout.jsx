e4ac536ddf2ba1a316fbb848ba555517067f4cad
AwaisAmin <awaisamin090@gmail.com>
2024-06-29 09:57:45 -0700

    added header

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,9 +1,11 @@
+import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <>
       <main className="min-h-screen container">
+        <Header />
         <Outlet />
       </main>
 
